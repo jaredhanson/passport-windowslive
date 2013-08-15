5249e3b8a1d792ae0e8d626595a47770b178a599
Jared Hanson <jaredhanson@gmail.com>
2013-08-14 21:51:03 -0700

    Test case for parsing failure.

--- test/strategy.profile.error.test.js
+++ test/strategy.profile.error.test.js
@@ -3,30 +3,29 @@ var WindowsLiveStrategy = require('../lib/strategy');
 
 describe('Strategy#userProfile', function() {
     
-  var strategy =  new WindowsLiveStrategy({
-      clientID: 'ABC123',
-      clientSecret: 'secret'
-    },
-    function() {});
+  describe('handling API errors', function() {
+    var strategy =  new WindowsLiveStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      },
+      function() {});
   
-    // mock
-    strategy._oauth2.get = function(url, accessToken, callback) {
-      if (url != 'https://apis.live.net/v5.0/me') { return callback(new Error('wrong url argument')); }
-      if (accessToken != 'token') { return callback(new Error('wrong token argument')); }
+      // mock
+      strategy._oauth2.get = function(url, accessToken, callback) {
+        if (url != 'https://apis.live.net/v5.0/me') { return callback(new Error('wrong url argument')); }
+        if (accessToken != 'token') { return callback(new Error('wrong token argument')); }
       
-      var body = '{ \
-           "error": { \
-              "code": "request_token_expired", \
-              "message": "The provided access token has expired." \
-           } \
-        }';
+        var body = '{ \
+             "error": { \
+                "code": "request_token_expired", \
+                "message": "The provided access token has expired." \
+             } \
+          }';
     
-      callback({ statusCode: 401, data: body });
-    }
-  
-  describe('encountering an API error', function() {
+        callback({ statusCode: 401, data: body });
+      }
+      
     var err, profile;
-    
     before(function(done) {
       strategy.userProfile('token', function(e, p) {
         err = e;
@@ -34,16 +33,43 @@ describe('Strategy#userProfile', function() {
         done();
       });
     });
-    
+  
     it('should error', function() {
       expect(err).to.be.an.instanceOf(Error);
       expect(err.constructor.name).to.equal('LiveConnectAPIError');
       expect(err.message).to.equal('The provided access token has expired.');
       expect(err.code).to.equal('request_token_expired');
     });
-    
-    it('should not load profile', function() {
-      expect(profile).to.be.undefined;
+  });
+  
+  describe.only('handling malformed responses', function() {
+    var strategy =  new WindowsLiveStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      },
+      function() {});
+  
+      // mock
+      strategy._oauth2.get = function(url, accessToken, callback) {
+        if (url != 'https://apis.live.net/v5.0/me') { return callback(new Error('wrong url argument')); }
+        if (accessToken != 'token') { return callback(new Error('wrong token argument')); }
+      
+        var body = 'Hello, world.';
+        callback(null, body, undefined);
+      }
+      
+    var err, profile;
+    before(function(done) {
+      strategy.userProfile('token', function(e, p) {
+        err = e;
+        profile = p;
+        done();
+      });
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal('Failed to parse user profile');
     });
   });
   
